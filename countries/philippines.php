ed12d59b5d8d7b63ac04b7c946fbbc3990e8cb44
Duncan <princedunc1214@gmail.com>
2025-12-13 20:51:33 +0800

    Update philippines.php

--- countries/philippines.php
+++ countries/philippines.php
@@ -1,6 +1,14 @@
 <p>amongus</p>
 <div>
     <p>
-        amogus amogus amogus
+       Philippines - Departure
+May 3, 1882 
+Manila, Philippines
+Rizal secretly leaves the Philippines for the first time on the Spanish steamer Salvadora, bound for Singapore, traveling under the name “Jose Mercado” to avoid suspicion from Spanish authorities and friars.
+His “secret mission” is to study in Spain, observe European societies, and prepare himself intellectually to work for reforms in the Philippines.
+Before his departure, he wrote a farewell letter for his beloved parents and sweetheart Leonor Rivera. 
+His departure was only known by his brother Paciano, his uncle Antonio Rivera, his sisters Neneng and Lucia, the Valenzuela family, Pedro A. Paterno, his compadre Mateo Evangelista, the Ateneo Jesuit fathers, and some intimate friends, including Chengoy or Jose M. Cecilio. 
+The Jesuit priests gave him letters of recommendation to the members of their Society in Barcelona.
+
     </p>
 </div>
