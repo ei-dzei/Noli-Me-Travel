56896da02e2f632575592fb4d7a59a5ee3603e0d
ga-briaa <gabd.amador@gmail.com>
2025-12-14 01:09:22 +0800

    Update countries

--- public/countries/singapore.php
+++ public/countries/singapore.php
@@ -15,10 +15,10 @@
             <span class="drop-cap">D</span>
             uring the voyage to Singapore, there were sixteen passengers including Rizal. There were five or six ladies, many children, and the rest were gentlemen. He was the only Filipino, others were Spaniards, British, and Indian Negroes.
             The ship captain, Donato Lecha from Asturias, Spain, befriended Rizal, whom he described as an affable man. However, he was peeved by some Spaniards who spoke ill of the Philippines.
-            To relieve his boredom, Rizal played chess with older passengers and won many times as he was a good chess player.
-            On May 8, 1882, while the steamer was approaching Singapore, Rizal saw a beautiful island that reminded him of “Talim Island with the Susong Dalaga”.
+            To relieve his boredom, Rizal played chess with older passengers and won many times as he was a good chess player. </br> </br>
+            On May 8, 1882, while the steamer was approaching Singapore, Rizal saw a beautiful island that reminded him of “Talim Island with the Susong Dalaga”. </br> </br>
             On May 9, 1882, the Salvadora docks in Singapore, where Rizal checks into Hotel de la Paz and spends about two days walking around the city, observing its busy harbor and multicultural streets.
-            Rizal saw the famous Botanical Garden, the beautiful Buddhist temples, the busy shopping district, and the statue of Sir Thomas Stanford Raffles, the founder of Singapore.
+            Rizal saw the famous Botanical Garden, the beautiful Buddhist temples, the busy shopping district, and the statue of Sir Thomas Stanford Raffles, the founder of Singapore. </br> </br>
             On May 11, 1882, Rizal transferred to the French steamer Djemnah, which left Singapore for Europe.
             Rizal found the Djemnah larger and cleaner, which carried more passengers; they were British, French, Dutch, Spaniards, Malays, Siamese, and Filipinos.
             The Filipinos who were with Rizal were Mr. and Mrs. Salazar and Mr. Vicente Pardo.
