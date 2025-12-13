4073dbea6f101a05c63b0f7db0d810e2f434306e
Duncan <princedunc1214@gmail.com>
2025-12-13 22:47:13 +0800

    Update italy.php

--- countries/italy.php
+++ countries/italy.php
@@ -1,7 +1,17 @@
 <div>
-    Italy
-June 11, 1882
-Naples, Italy
-On June 11, 1882, Rizal reached Naples, which pleased him because of its business activity, lively people, and its panoramic beauty. He was captivated by Mount Vesuvius, the Castle of St. Telmo, and other historic sights of the city.
+    <h2> Italy </h2> 
+    <h3>June 11, 1882 <br>
+        Naples, Italy    
+    </h3>
+    <p>
+        <ul>
+            <li>
+    On the night of June 12, 1882, Djemnah docked at the French harbor of Marseilles. Rizal visited the famous Château d’lf, where Dantes, hero of The Count of Monte Cristo, was imprisoned.
+            </li>
+            <li>
+    Rizal stayed for two and a half days in Marseilles.
+            </li>
+        </ul>
+    </p>
 
 </div>
