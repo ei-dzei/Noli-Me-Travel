f708371de3dce0be59ba9e0373c40cac78b3d817
ga-briaa <gabd.amador@gmail.com>
2025-12-13 23:12:12 +0800

    Updated format of Egypt

--- countries/egypt.php
+++ countries/egypt.php
@@ -1,13 +1,23 @@
-<div>
-    <h2>Egypt</h2> 
-    <h3>Late May 1882</h3>
-    <h3>City of Suez (Red Sea terminal of the Suez Canal)</h3>
-    <p>
-        Rizal disembarked and went sightseeing, and was most impressed by the beautiful moonlight, which reminded him of Calamba and his family.
-        Djemnah took five days to traverse through the Suez Canal, which was built by Ferdinand de Lesseps, a French diplomat-engineer, and was inaugurated on November 17, 1869.
-    </p>
-    <h3>Port Said</h3>
-    <p>
-     Rizal landed in Port Said, the Mediterranean terminal of the Suez Canal, to see the interesting sights. He was fascinated by the multi-racial inhabitants, speaking a babel of tongues like Arabic, Egyptian, Greek, French, Italian, Spanish, etc.
-    </p>
+<div class="journal-entry">
+    <h1 class="elegant-title">Egypt</h1>
+    <h2 class="vintage-subtitle">Late May 1882</h2>
+    <h2 class="vintage-subtitle">Suez City (Red Sea terminal of the Suez Canal), Egypt</h2>
+
+    <div class="text-columns single-col">
+        <p>
+            <span class="drop-cap">R</span>
+            izal disembarked and went sightseeing, and was most impressed by the beautiful moonlight, which reminded him of Calamba and his family.
+            Djemnah took five days to traverse through the Suez Canal, which was built by Ferdinand de Lesseps, a French diplomat-engineer, and was inaugurated on November 17, 1869.
+        </p>
+    </div>
+
+    <h2 class="vintage-subtitle"></h2>
+    <h2 class="vintage-subtitle">Port Said, Egypt</h2>
+
+    <div class="text-columns single-col">
+            <p>
+                <span class="drop-cap">R</span>
+                izal landed in Port Said, the Mediterranean terminal of the Suez Canal, to see the interesting sights. He was fascinated by the multi-racial inhabitants, speaking a babel of tongues like Arabic, Egyptian, Greek, French, Italian, Spanish, etc.
+            </p>
+    </div>
 </div>
