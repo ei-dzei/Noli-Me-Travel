713f6f3326e80ba033f4317f51402c29b4617c07
Duncan <princedunc1214@gmail.com>
2025-12-13 23:03:46 +0800

    Update spain.php

--- countries/spain.php
+++ countries/spain.php
@@ -1,13 +1,9 @@
-<div>
-    Spain - Arrival
-June 16, 1882
-On the afternoon of June 15, 1882, Rizal left Marseille by train.
-Rizal crossed the Pyrenees and stopped for a day at the frontier town of Port Bou, where he noticed the indifference of Spanish immigration officers to tourists, unlike the French immigration officers.
-On June 16, 1882, Rizal reached his destination, Barcelona, by rail.
-Rizal’s first impression of Barcelona, the greatest city of Cataluña and Spain’s second largest city, was ugly and dirty due to the little inns and inhospitable residents.
-Rizal happened to stay upon his arrival at a dark and dirty inn located on an unimpressive narrow street in the town’s most ugly side, where staff and guests were indifferent to him. 
-Later, Rizal found the city to be great with its atmosphere of freedom and liberalism. Its people were open-hearted, hospitable, and courageous.
-Rizal enjoyed promenading along Las Ramblas, the most famous street in Barcelona.
-Rizal was welcomed by his schoolmates in Ateneo. They gave him a party at their favorite cafe in Plaza de Cataluña, where they had an exchange of toasts and told him about the attractions of Barcelona and the customs of the Spanish people.
+<div class="journal-entry">
+    <h1 class="elegant-title">Spain</h1>
+    <h2 class="vintage-subtitle">June 16, 1882</h2>
+    <div class="text-columns single-col">
+            <span class="drop-cap">O</span>
+<p>
 
+</p>
 </div>
