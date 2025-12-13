7a44791d0154289ae92e88ad1828491b3e2d0798
ei-dzei <155649754+ei-dzei@users.noreply.github.com>
2025-12-13 23:30:01 +0800

    kasanalan natin lahat ito (or ako)

---
+++ public/countries/italy.php
@@ -0,0 +1,36 @@
+<div class="journal-entry">
+    <div class="passport-stamp-container">
+        <div class="stamp-ink">
+            <span class="stamp-top">VISITED</span>
+            <span class="stamp-center">ITALY</span>
+            <span class="stamp-bottom">June 11, 1882</span>
+        </div>
+    </div>
+
+    <h1 class="elegant-title"> Italy </h1>
+    <h2 class="vintage-subtitle">June 11, 1882</h2>
+    <h2 class="vintage-subtitle">Naples, Italy </h2>
+   
+    <div class="text-columns single-col">
+            <span class="drop-cap">O</span>
+    <p>
+    n the night of June 12, 1882, Djemnah docked at the French harbor of Marseilles. Rizal visited the famous Château d’lf, where Dantes, hero of The Count of Monte Cristo, was imprisoned.<br>
+    Rizal stayed for two and a half days in Marseilles.
+    </p>
+    </div>
+
+    <div class="assets-section gallery-row">
+        <div class="gallery-item" onclick="openassetsModal('Château d’If', 'The famous fortress-prison featured in Alexandre Dumas\' novel, The Count of Monte Cristo. Rizal visited this site as a fan of the book.', './assets/yemen-aden/aden-city.png')">
+            <img src="assets\italy\castle-st-elmo.png" alt="Château d’If">
+        </div>
+
+        <div class="gallery-item" onclick="openassetsModal('Port of Marseilles', 'Rizal was impressed by the busy and modern harbor.', './assets/yemen-aden/camels.png')">
+            <img src="assets\italy\mount-vesuvius.png" alt="Port of Marseilles">
+        </div>
+
+        <div class="gallery-item" onclick="openassetsModal('Express Train', 'Rizal took an express train crossing the Pyrenees mountains to reach Barcelona, Spain.', './assets/yemen-aden/mountains.png')">
+            <img src="assets\italy\naples-city.png" alt="Train in France">
+        </div>
+    </div>
+
+</div>
