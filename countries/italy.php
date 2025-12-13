f276981d6be55c19e4c0a6f5a32d093fbb3ad3b5
Duncan <princedunc1214@gmail.com>
2025-12-13 23:15:40 +0800

    Update italy.php

--- countries/italy.php
+++ countries/italy.php
@@ -1,27 +1,9 @@
-<div>
-    <h2> Italy </h2> 
-    <h3>June 11, 1882 <br>
-        Naples, Italy    
-    </h3>
-    <p>
-        <ul>
-            <li>
-    On the night of June 12, 1882, Djemnah docked at the French harbor of Marseilles. Rizal visited the famous Château d’lf, where Dantes, hero of The Count of Monte Cristo, was imprisoned.
-            </li>
-            <li>
- 
-            </li>
-        </ul>
-    </p>
-
-</div>
-
 <div class="journal-entry">
     <h1 class="elegant-title"> Italy </h1>
     <h2 class="vintage-subtitle">June 11, 1882</h2>
     <h2 class="vintage-subtitle">Naples, Italy </h2>
     <div class="text-columns single-col">
-            <span class="drop-cap">)</span>
+            <span class="drop-cap">O</span>
     <p>
     n the night of June 12, 1882, Djemnah docked at the French harbor of Marseilles. Rizal visited the famous Château d’lf, where Dantes, hero of The Count of Monte Cristo, was imprisoned.<br>
     Rizal stayed for two and a half days in Marseilles.
