0b11a34cd006f11e3032acccf6e203dff25c8fa1
Duncan <princedunc1214@gmail.com>
2025-12-13 22:29:31 +0800

    update philippines and ceylon

--- countries/philippines.php
+++ countries/philippines.php
@@ -4,12 +4,10 @@
         Manila, Philippines
     </h3>
     <p>
-      
 Rizal secretly leaves the Philippines for the first time on the Spanish steamer Salvadora, bound for Singapore, traveling under the name “Jose Mercado” to avoid suspicion from Spanish authorities and friars.
 His “secret mission” is to study in Spain, observe European societies, and prepare himself intellectually to work for reforms in the Philippines.
 Before his departure, he wrote a farewell letter for his beloved parents and sweetheart Leonor Rivera. 
 His departure was only known by his brother Paciano, his uncle Antonio Rivera, his sisters Neneng and Lucia, the Valenzuela family, Pedro A. Paterno, his compadre Mateo Evangelista, the Ateneo Jesuit fathers, and some intimate friends, including Chengoy or Jose M. Cecilio. 
 The Jesuit priests gave him letters of recommendation to the members of their Society in Barcelona.
-
     </p>
 </div>
