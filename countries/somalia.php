12779c65c84537171a1808407c7f9813a2ae36ca
Duncan <princedunc1214@gmail.com>
2025-12-13 23:12:39 +0800

    Update somalia.php

--- countries/somalia.php
+++ countries/somalia.php
@@ -1,9 +1,23 @@
-<div>
-    <h2> Somalia </h2>
-    <h3> Late May, 1882<br>
-        Crossing Indian Ocean to the Cape of Guardafui, Africa (Somalia)
-    </h3>
+<div class="journal-entry">
+    <h1 class="elegant-title"> Somalia </h1>
+    <h2 class="vintage-subtitle">Late May, 1882</h2>
+    <h2 class="vintage-subtitle">Crossing Indian Ocean to the Cape of Guardafui, Africa (Somalia)
+    </h2>
+    <div class="text-columns single-col">
+            <span class="drop-cap">R</span>
     <p>
-        Rizal sighted the barren coast of Africa, which he called an inhospitable land but famous.
+        izal sighted the barren coast of Africa, which he called an inhospitable land but famous.
     </p>
 </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
